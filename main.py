ceddd2853a4733ee91095eed34d4150e699f3540
deansareut <deanandriaccio@proton.me>
2026-08-20 18:42:51 -0400

    Add indicator for when bot is online

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix=":", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is online.")
