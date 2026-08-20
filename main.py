6066457ca2ccfc6bd150f5e42dae7ef1d0bc200f
deansareut <deanandriaccio@proton.me>
2026-08-20 18:37:46 -0400

    Add necessary intents

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
